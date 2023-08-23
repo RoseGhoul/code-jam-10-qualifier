7792b88796a9e225a9020b975570f81ac4d5d429
RoseGhoul <142997893+RoseGhoul@users.noreply.github.com>
2023-08-23 20:01:19 +0530

    Update qualifier.py

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,20 +1,49 @@
-def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
-    """
-    Return True if the given input allows the rearrangement of the image, False otherwise.
+from PIL import Image
+#Enter paths using //
+input_path=input("Enter input path=")
+output_path=input("Enter output path=")
+im=Image.open(input_path)
+w,h=im.size
+#Enter Orderlist
+order=[]
+a=[]
+b=[]
+tw=int(input("Enter tile width="))
+th=int(input("Enter tile height"))
+div1=w/tw
+div2=h/th
+n=div1*div2
 
-    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
-    once.
-    """
+def valid_input(image_size=(w,h), tile_size=(tw,th), ordering=order):
+    div1=w/tw
+    div2=h/th
+    if div1%int(div1)==0 and div2%int(div2)==0:
+        return True
+    else:
+        return False
+        
+def rearrange_tiles(image_path=input_path, tile_size=(tw,th), ordering=order, out_path=output_path): 
+    if len(ordering)!=len(set(ordering)):
+        raise ValueError("The tile size or ordering are not valid for the given image")
+    else:
+        for i in range(0,int(div2)):
+            for j in range(0,int(div1)):
+                pic=im.crop(box=(0+j*(w/div1),0+i*(w/div1),0+(j+1)*(w/div1),0+(i+1)*(w/div1)))
+                loc=[0+j*(w/div1),0+i*(w/div1),0+(j+1)*(w/div1),0+(i+1)*(w/div1)]
+                a.append(pic)
+                b.append(loc)
+        
+        for i in a:
+            i=i.resize((tw,th))
+        
+        new_img=Image.new("RGBA",(w,h))
+        for i in range(int(n)):
+            new_img.paste(a[order[i]],(int(b[i][0]),int(b[i][1])))
+            
+        new_img.save(output_path+"\\ARRANGED.png")
 
-
-def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
-    """
-    Rearrange the image.
-
-    The image is given in `image_path`. Split it into tiles of size `tile_size`, and rearrange them by `ordering`.
-    The new image needs to be saved under `out_path`.
-
-    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
-    once. If these conditions do not hold, raise a ValueError with the message:
-    "The tile size or ordering are not valid for the given image".
-    """
+condition=valid_input()
+if condition==True:
+    rearrange_tiles()
+else:
+    print("Quitting")
